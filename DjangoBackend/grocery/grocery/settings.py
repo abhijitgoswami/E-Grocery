6f4f660e19adc60653da57bf3b16c98ee4f84802
abhijitgoswami <abhijeetgoswami45@gmail.com>
2020-12-22 19:19:36 +0530

    second

--- DjangoBackend/grocery/grocery/settings.py
+++ DjangoBackend/grocery/grocery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qb!*y%s8hoqk!th(n16&)1c75o=^1$xl7wx4^=^kdsxl(0+gtf'
+#SECRET_KEY = 'qb!*y%s8hoqk!th(n16&)1c75o=^1$xl7wx4^=^kdsxl(0+gtf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
